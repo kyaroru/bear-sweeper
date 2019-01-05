da87793d6e8d2c81cfdf992a8c56a9213b2a0ee4
Carol <kyaroru@outlook.com>
2019-01-05 10:59:45 +0800

    Do not display AppStore & GooglePlay icon for Window Platform

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,6 +5,8 @@ import googleplay from '../images/googleplay.png';
 import icon from '../images/icon.png';
 import * as Colors from '../themes/colors';
 
+var process = window.process;
+
 const styles = {
   backButton: {
     position: 'absolute',
@@ -24,14 +26,17 @@ const Difficulty = () => (
         BearSweeper is originally build in React Native!
       </div>
       <img className="icon" alt="brownie" src={icon} />
-      <div>
+      {process.platform !=='win32' && <div>
         <a target="_blank" rel="noopener noreferrer" href="https://itunes.apple.com/us/app/bearsweeper/id1447809603?ls=1&mt=8">
           <img className="appstore-icon" alt="appstore" src={appstore} />
         </a>
         <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.bearsweeper&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
           <img className="google-icon"  alt="googleplay" src={googleplay} />
         </a>
-      </div>
+      </div>}
+      {process.platform === 'win32' &&
+        <div>And it is now ported to ElectronJS :p</div>
+      }
       <div style={styles.backButton}>
         <Link to="/" style={styles.link}>⬅ Back</Link>
       </div>
